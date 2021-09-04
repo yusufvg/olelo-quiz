45f984bc3a5a75817abdca5f5a4bd5e8ea1f512d
Yusuf Van Gieson <yusufvg98@gmail.com>
2021-09-04 15:53:48 -0700

    added educational resources

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,10 +2,29 @@ import React from "react";
 
 const NavBar = ({ score }) => {
   return (
-    <nav className="navbar navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <h1 className="navbar-brand">Hō'ike Mo'omeheu Hawai'i</h1>
-        <div className="card">
+        {/* title */}
+        <h1 className="me-auto navbar-brand">Hō'ike Mo'omeheu Hawai'i</h1>
+
+        {/* educational links */}
+        {/* TODO handle collapse on smaller breakpoints */}
+
+        <ul className="navbar nav">
+          <li className="nav-item">
+            <a href="https://www.instagram.com/ealaehcc/" className="nav-link">
+              E Ala E Hawaiian Cultural Center
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="http://wehewehe.org/" className="nav-link">
+              Nā Puke Wehe Wehe 'Õlelo Hawai'i
+            </a>
+          </li>
+        </ul>
+
+        {/* score card */}
+        <div className="nav-item card m-x1">
           <p className="m-2">
             <span className="badge bg-primary rounded-pill m-2">
               {score} Points
